32827f85c12cec39ecf5c008b190c44493638d56
Larissa <larissadecastro18@gmail.com>
2022-06-26 17:20:42 -0300

    adicionando resolução do dia 1 do desafio

---
+++ LogicaJS/dia1.js
@@ -0,0 +1,24 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (typeof numeroUm ==='number' && typeof stringUm ==='string') {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+if (typeof numeroTrinta === 'number' && typeof stringTrinta === 'string') {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+if (typeof numeroDez ==='number' && typeof stringDez === 'string') {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
